7abfc96fcefbc5b4397c2e94d04051a4059ec8d6
maxc0434 <max.couillet@gmail.com>
2025-11-12 15:24:02 +0100

    type -> interface product ok

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,7 +1,10 @@
 import HomeHeader from "@/components/HomeHeader";
+import { useState } from "react";
 import { View, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
+  // featuredProducts : state pour les produits favoris
+  const [featuredProducts, setFeaturedProducts] = useState([]);
   return (
     
       <View>
